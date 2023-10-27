62be5382cae55da76581eb53ad23ef91f2b60e69
parkchangseon <abc1234@gmail.com>
2023-10-27 11:12:13 +0900

    예약기능추가

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,6 +1,7 @@
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.UUID;
 
 public class Reservation {
@@ -25,5 +26,18 @@ public class Reservation {
         String id = UUID.randomUUID().toString();
         this.reserved_list.add(new Reservation(id, room, customer));
         return id;
+    }
+    public void addReservation(Hotel hotel){
+        System.out.println("예약하시겠습니까?");
+        System.out.println("1. 예" + "2. 아니오");
+        Scanner sc  = new Scanner(System.in);
+        if (sc.nextInt()==1){
+            System.out.println("예약하실 방 번호를 입력해주세요");
+            int input = sc.nextInt();
+            ((Room) hotel.getRoomList().get(input-1)).showRoom(input);
+            reserve((Room) hotel.getRoomList().get(input-1),hotel.getReserving_customer());
+            System.out.println(hotel.getReserving_customer().getName());
+            hotel.start();
+        }
     }
 }
